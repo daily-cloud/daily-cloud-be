7c6ad7357d13616a05b4c09efeffde6199d36373
fhrlzmn <fhrlzmn26@gmail.com>
2023-06-10 22:23:00 +0700

    [DEBUG] Fix firebase service account for development

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,14 @@
 const admin = require('firebase-admin');
-// const path = require('path');
+const path = require('path');
+
+const serviceAccount = require(path.join(
+  __dirname,
+  '../../serviceAccount.json'
+));
 
 // check the code running on CLOUD RUN (without service account) or LOCAL
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = admin;
